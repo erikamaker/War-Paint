e2d5892ba7dcfb4d32151468b111f1275c380d5c
Ian Terrell <cvz5an@virginia.edu>
2023-03-28 23:24:54 -0400

    Extract game loop to renderer

--- lib/terminal_renderer.rb
+++ lib/terminal_renderer.rb
@@ -1,7 +1,21 @@
 class TerminalRenderer
+  def initialize(board)
+    @board = board
+  end
+
+  def play!
+    loop do
+      display(@board.grid(empty_square))      # Curses instance creates a grid of the preset for empty squares, defined in Curses.
+      print "Select `Q` to Quit  >>  "                         # Prompt for...
+      input = gets.chomp.downcase                              # ... and accept input.
+      break if input == 'q'                                    # Exit program.
+    end
+  end
+
   def display(squares)
     squares.each {|row| puts row.join}                     # For each element in coords, join as a string and output it with a new line.
   end
+
   def empty_square
     "⬚ "
   end
