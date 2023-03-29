085947d48afa97f147f4429fe195ef00fdafaaf1
Ian Terrell <cvz5an@virginia.edu>
2023-03-29 00:04:28 -0400

    Play with Curses renderer
        ./bin/war_paint
        ./bin/war_paint -c

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,12 @@ class Board
   def initialize(rows, cols)
     @grid = Array.new(rows) {Array.new(cols, Cell.new) }
   end
+
+  def height
+    @grid.length
+  end
+
+  def width
+    @grid.first.length
+  end
 end
--- lib/war_paint.rb
+++ lib/war_paint.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
 require_relative 'board'
 require_relative 'cell'
+require_relative 'curses_renderer'
 require_relative 'terminal_renderer'
