16a3ae9537d36b1dbec388807eb4e4cc357e891e
Erika Maker <erikamaker@outlook.com>
2023-04-27 22:51:53 -0400

    Get a basic CPU running

---
+++ lib/cpu.rb
@@ -0,0 +1,15 @@
+class Cpu
+    def initialize(board)
+        @board = board
+    end
+    def cpus_turn
+        @board.grid.each_with_index do |row, row_index|
+            row.each_with_index do |cell, column_index|
+                if !cell.painted?
+                    @board.paint!(row_index,column_index)
+                    return
+                end
+            end
+        end
+    end
+end
